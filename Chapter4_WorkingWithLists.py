2068587dea9ac1c2173921f8d87f15c806ebd646
chanXVI <spannachan@gmail.com>
2020-04-27 19:37:32 -0500

    Update Chapter4_WorkingWithLists.py

--- Chapter4_WorkingWithLists.py
+++ Chapter4_WorkingWithLists.py
@@ -11,5 +11,156 @@ for team in nfl_teams:
 
 print('The teams above will tank for the first four picks :(')
 
-
+######################################################################################################
 # Page 56: Try it yourself!!!
+# Add PORTIONS FROM MACBOOK PRO
+######################################################################################################
+
+# # Try it yourself
+# # 4-3
+# for x in range(1,21):
+#   print(x)
+
+# # 4-4
+# big_list = []
+# for num in range (2,1_000_000):
+#   big_list.append(num)
+# print(big_list)
+
+# # list comprehension to do the same thing above
+# big_list = [x for x in range(1,1_000_001)]
+
+# for x in big_list:
+#   print(x)
+
+# # 4-5
+# big_list = [x for x in range(1,1_000_001)]
+# print(min(big_list))
+# print(max(big_list))
+# print(sum(big_list))
+
+# # 4-6
+# odd_list = [x for x in range(1,20,2)]
+# print(odd_list)
+# for y in odd_list:
+#     print (y)
+
+# # 4-7
+# mult_3_list = [x for x in range(3,31,3)]
+# print(mult_3_list)
+
+# # 4-8 and 4-9
+
+# #doing the cube using list comprehension
+# cube_list = [x**3 for x in range(1,11)]
+# print(cube_list)
+
+# # doing the cube using the long way
+# cube_list2 = []
+# for num in range (1,11):
+#    cube_list2.append(num**3)
+# print(cube_list2)
+
+# # Slicing a List
+# mlb_teams = ['brewers', 'reds', 'cubs', 'pirates', 'yankees']
+# print(mlb_teams[0:3])
+# print(mlb_teams[-3:])
+
+# # looping through a slice
+# nba_team = ['bulls', 'bucks', 'knicks', 'thunder', 'warriors']
+
+# print('My top three basketball teams are:')
+# for team in nba_team[:3]:
+#     print(team.title())
+
+# # copying a list
+# shaq_teams = ['magic', 'lakers']
+# dwight_teams = shaq_teams[:]
+# print('Shaq has played for')
+# print(shaq_teams)
+# print('\nDwight has also played for')
+# print(dwight_teams)
+
+# shaq_teams.append('heat')
+# dwight_teams.append('rockets')
+
+# print('\nShaq has played for')
+# print(shaq_teams)
+# print('\nDwight has played for')
+# print(dwight_teams)
+
+# # 4-10
+# baseball_player = []
+# baseball_player.append('jeter')
+# baseball_player.append('arod')
+# baseball_player.append('big_papi')
+# baseball_player.append('manny')
+# baseball_player.append('braun')
+# baseball_player.append('prince')
+# baseball_player.append('griffey')
+# baseball_player.append('dimmagio')
+# baseball_player.append('aaron')
+# baseball_player.append('mantle')
+
+
+# print('The first three items in the list are:')
+# for player in baseball_player[:3]:
+#     print(player)
+
+# print('Three player from the middle of the list are:')
+# for player in baseball_player[4:7]:
+#     print(player)
+
+# print('The last three items in the list are:')
+# for player in baseball_player[-3:]:
+#     print(player)
+
+# # 4-11
+
+# pizza = ['pep', 'sausage', 'bacon']
+# friends_pizza = pizza[:]
+
+# pizza.append('pineapple')
+# friends_pizza.append('anchovies')
+
+# print('My favorite pizza are:')
+# for flavor in pizza:
+#     print(flavor)
+
+# print("\nMy friend's favorite pizza are:")
+# for flavor in friends_pizza:
+#     print(flavor)
+
+# # TUPLES
+# # Defining a Tuple
+# # Dimentions.py
+
+# dimention = (200, 50)
+# print(dimention[0])
+# print(dimention[1])
+
+# my_t = (1,)
+# print(type(my_t))
+
+# for size in dimention:
+#     print(size)
+
+# # writing over a tuple
+# dimentions = (200, 50)
+# print('Original dimentions:')
+# for dimention in dimentions:
+#     print(dimention)
+
+# dimentions = (400, 100)
+# print("\nModified dimentions:")
+# for dimention in dimentions:
+#     print(dimention)
+
+# # 4-13
+# buffet_staple = ('rice', 'chicken', 'soy sause', 'broccoli', 'fortune cookie')
+# for food in buffet_staple:
+#     print(food)
+# print('\n')
+# buffet_staple = ('rice', 'beef', 'soy sause', 'noodles', 'fortune cookie')
+# for food in buffet_staple:
+#     print(food)
