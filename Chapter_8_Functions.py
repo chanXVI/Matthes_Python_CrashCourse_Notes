d8a8cdba53421a766e94581087802b7c9b5dad7a
chanXVI <spannachan@gmail.com>
2020-05-05 21:04:32 -0500

    Update Chapter_8_Functions.py

--- Chapter_8_Functions.py
+++ Chapter_8_Functions.py
@@ -98,4 +98,152 @@
 # describe_city('manchester')
 # describe_city('paris')
 ##############################################################################
-# Start return values: page 137
+# # Return Values
+
+# # returning a simple value
+# def get_formatted_name(first_name, last_name):
+#     """Return a full name, neatly formatted."""
+#     full_name = f"{first_name} {last_name}"
+#     return full_name.title()
+
+# musician = get_formatted_name('jimi', 'hendrix')
+# print(musician)
+##############################################################################
+# # making an argument optional
+# def get_formatted_name(first_name, last_name, middle_name = ''):
+#     """ Return a full name, neatly formatted"""
+#     if middle_name:
+#         full_name = f"{first_name} {middle_name} {last_name}"
+#     else:
+#         full_name = f"{first_name} {last_name}"
+#     return full_name.title()
+
+# musician = get_formatted_name('jimi', 'hendrix')
+# print(musician)
+
+# musician = get_formatted_name('stevie', 'vaughan', 'ray')
+# print(musician)
+##############################################################################
+# # returning a dictionary
+# def build_person(first_name, last_name):
+#     """Return a dictionary of information about a person."""
+#     person = {'first': first_name, 'last': last_name}
+#     return person
+
+# musician = build_person('jimi', 'hendrix')
+# print(musician)
+# ##############################################################################
+# def build_person(first_name, last_name, age = None):
+#     """Return a dictionary of information about a person."""
+#     person = {'first': first_name, 'last': last_name}
+#     if age:
+#         person['age'] = age
+#     return person
+
+# musician = build_person('jimi', 'hendrix', age = 27)
+# print(musician)
+# ##############################################################################
+# # Using a function with a while loop
+# def get_formatted_name(first_name, last_name):
+#     """Return a full name, neatly formatted."""
+#     full_name = f"{first_name} {last_name}"
+#     return full_name.title()
+# # This is an infinite loop
+# while True:
+#     print("\nPlease tell me your name:")
+#     f_name = input("First name: ")
+#     l_name = input("Last name: ")
+
+#     formatted_name = get_formatted_name(f_name, l_name)
+#     print(f"\nHello {formatted_name}!")
+
+# # function with a while loop with quit condition:
+# def get_formatted_name(first_name, last_name):
+#     """Return a full name, neatly formatted."""
+#     full_name = f"{first_name} {last_name}"
+#     return full_name.title()
+
+# while True:
+#     print("\nPlease tell me your name:")
+#     print("(Enter 'q' at any time to quit)")
+
+#     f_name = input("First name: ")
+#     if f_name == 'q':
+#         break
+
+#     l_name = input("Last name: ")
+#     if l_name == 'q':
+#         break
+
+#     formatted_name = get_formatted_name(f_name, l_name)
+#     print(f"\nHello {formatted_name}!")
+# ##############################################################################
+# # Try it Yourself
+
+# # 8-6
+# def city_country(city, country):
+#     """Returns whatever argument is given for the paramaters city and country"""
+#     location = f"{city}, {country}"
+#     return location.title()
+
+# place_1 = city_country('paris', 'france')
+# place_2 = city_country('berlin', 'germany')
+# place_3 = city_country('london', 'england')
+
+# total_place = [place_1, place_2, place_3]
+
+# for place in total_place:
+#     print(f"\n{place}")
+##############################################################################
+# # 8-7
+# def make_album(artist_name, album_title, number_of_songs = None):
+#     """ Returning a dictionary containing an artist name and their\
+#      album title"""
+#     album = {'name': artist_name.title(), 'album title': album_title.title()}
+
+#     if number_of_songs:
+#         album['number of songs'] = number_of_songs
+    
+#     return album
+
+# album_1 = make_album('kanye west', 'graduation', 13 )
+# album_2 = make_album('john mayer', 'continuum')
+
+# albums = [album_1, album_2]
+# for album in albums:
+#     print(album)
+# ##############################################################################
+# # 8-8
+# def make_album(artist_name, album_title, number_of_songs = None):
+#     """ Returning a dictionary containing an artist name and their\
+#      album title"""
+#     album = {'name': artist_name.title(), 'album title': album_title.title()}
+
+#     if number_of_songs:
+#         album['number of songs'] = number_of_songs
+    
+#     return album
+
+# while True:
+#     print("Welcome to  the record store!!!")
+#     print("If you are don't need any help, please write 'done': ")
+
+#     artist = input("What is the name of the artist? ")
+
+#     if artist == 'done':
+
+#         print("Thank You for shopping at the record store.")
+#         break
+
+#     album = input(f"What is the name of the {artist.title()}'s album? ")
+
+#     if album == 'done':
+
+#         print("Thank You for shopping at the record store!")
+#         break
+
+#     catalog = make_album(artist, album)
+    
+#     print(catalog)
+# ##############################################################################
+
