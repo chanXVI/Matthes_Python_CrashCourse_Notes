1e7ba3f5d7c1d3d253f0a3c8b31a7450f377bd7a
chanXVI <spannachan@gmail.com>
2020-05-10 01:06:11 -0500

    Update Chapter_9_Classes.py

--- Chapter_9_Classes.py
+++ Chapter_9_Classes.py
@@ -24,7 +24,6 @@
 # my_dog = Dog('Willie', 6)
 # print(f"My dog's name is {my_dog.name}.")
 # print(f"My dog is {my_dog.age} years old.")
-    
 #     # accesing attributes
 # print(my_dog.name.upper())
     
@@ -148,4 +147,186 @@
 #     player.describe_user()
 #     player.jersey_number()
 # ##############################################################################
-# page 162: Working with classes and instances
+# # working with classes and instances
+# class Car: 
+# 	"""A simple attempt to represent a car"""
+
+# 	def __init__(self, manufacturer, model, year):
+# 		"""Initialize attributes to describe a car"""
+# 		self.manufacturer = manufacturer
+# 		self.model = model
+# 		self.year = year
+# 		# setting a default value for a attribute
+# 		self.odometer_reading = 0
+
+# 	def get_descriptive_name(self):
+# 		"""Return a neatly formatted descriptive name"""
+# 		long_name = f"{self.year} {self.manufacturer} {self.model}"
+# 		return long_name.title()
+
+# 	def read_odomenter(self):
+# 		"""Print a statement showing the car's milage"""
+# 		print(f"This car has {self.odometer_reading} miles on it.")
+
+# my_new_car = Car('audi', 'a4', 2019)
+# print(my_new_car.get_descriptive_name())
+# my_new_car.read_odomenter()
+
+# # modifying attrubutes value
+# 	# modifying an attribute value directly
+# my_new_car.odometer_reading = 23
+# my_new_car.read_odomenter()
+# ##############################################################################
+# # modifying an attribute's value through a method
+# class Car: 
+# 	"""A simple attempt to represent a car"""
+
+# 	def __init__(self, manufacturer, model, year):
+# 		"""Initialize attributes to describe a car"""
+# 		self.manufacturer = manufacturer
+# 		self.model = model
+# 		self.year = year
+# 		# setting a default value for a attribute
+# 		self.odometer_reading = 0
+
+# 	def get_descriptive_name(self):
+# 		"""Return a neatly formatted descriptive name"""
+# 		long_name = f"{self.year} {self.manufacturer} {self.model}"
+# 		return long_name.title()
+
+# 	def read_odomenter(self):
+# 		"""Print a statement showing the car's milage"""
+# 		print(f"This car has {self.odometer_reading} miles on it.")
+
+# 	def update_odometer(self, milage):
+# 		"""
+# 		Set the odometer reading to the given value.
+# 		Reject the change if it attempts to roll the odometer back.
+# 		"""
+# 		if milage >= self.odometer_reading:
+# 			self.odometer_reading = milage
+# 		else:
+# 			print("You can't roll back an odometer!")
+
+
+# my_new_car = Car('audi', 'a4', 2019)
+# print(my_new_car.get_descriptive_name())
+
+# my_new_car.update_odometer(23)
+# my_new_car.read_odomenter()
+
+# my_new_car.update_odometer(20)
+# my_new_car.read_odomenter()
+
+# my_new_car.update_odometer(25)
+# my_new_car.read_odomenter()
+# ##############################################################################
+# # incrementing an attribute's value through a method
+# class Car: 
+# 	"""A simple attempt to represent a car"""
+
+# 	def __init__(self, manufacturer, model, year):
+# 		"""Initialize attributes to describe a car"""
+# 		self.manufacturer = manufacturer
+# 		self.model = model
+# 		self.year = year
+# 		# setting a default value for a attribute
+# 		self.odometer_reading = 0
+
+
+# 	def get_descriptive_name(self):
+# 		"""Return a neatly formatted descriptive name"""
+# 		long_name = f"{self.year} {self.manufacturer} {self.model}"
+# 		return long_name.title()
+
+
+# 	def read_odomenter(self):
+# 		"""Print a statement showing the car's milage"""
+# 		print(f"This car has {self.odometer_reading} miles on it.")
+
+
+# 	def update_odometer(self, milage):
+# 		"""
+# 		Set the odometer reading to the given value.
+# 		Reject the change if it attempts to roll the odometer back.
+# 		"""
+# 		if milage >= self.odometer_reading:
+# 			self.odometer_reading = milage
+# 		else:
+# 			print("You can't roll back an odometer!")
+
+
+# 	def increment_odometer(self, miles):
+# 		""" 
+# 		Add the given amount to the odometer reading.
+# 		Reject the change if it attempts to roll the odometer back.
+# 		"""
+# 		if miles >= 0:
+# 			self.odometer_reading += miles
+# 		else:
+# 			print("You can't roll back an odometer!")
+
+
+# my_used_car = Car('subaru', 'outback', 2015)
+# print(my_used_car.get_descriptive_name())
+
+# my_used_car.update_odometer(23_500)
+# my_used_car.read_odomenter()
+
+# my_used_car.increment_odometer(100)
+# my_used_car.read_odomenter()
+
+# my_used_car.increment_odometer(-50)
+# my_used_car.read_odomenter()
+# ##############################################################################
+# # try it yourself
+# # 9-4
+# class Restaurant:
+# 	"""A description of a restaurant"""
+
+# 	def __init__(self, restaurant_name, cuisine_type):
+# 		"""the name of the restaurant and it's cuisine type"""
+# 		self.restaurant_name = restaurant_name
+# 		self.cuisine_type = cuisine_type
+# 		self.number_served = 0
+
+
+# 	def describe_restaurant(self):
+# 		"""two piece of information about restaurant"""
+# 		print(f"{self.restaurant_name.title()} is located in the United States.")
+# 		print(f"They offer dine-in, takeout and delivery.")
+
+
+# 	def open_restaurant(self):
+# 		"""print a message indicating that the restaurant is open"""
+# 		print(f"{self.restaurant_name.title()} is currently open.")
+
+
+# 	def set_number_served(self, customers_served):
+# 		self.number_served = customers_served
+
+# 	def increment_number_served(self, customers_served_this_order):
+# 		if customers_served_this_order >= 0:
+# 			self.number_served += customers_served_this_order
+
+# 		else:
+# 			print("You can't serve a negative amount of customers")
+
+
+
+# restaurant_1 = Restaurant('sabri', 'indian and pakistani')
+# print(restaurant_1.number_served)
+# restaurant_1.number_served = 4
+# print(restaurant_1.number_served)
+
+
+# restaurant_1.set_number_served(5)
+# print(restaurant_1.number_served)
+
+# restaurant_1.increment_number_served(5)
+# restaurant_1.increment_number_served(2)
+# restaurant_1.increment_number_served(3)
+
+# print(f"Today we served a total of {restaurant_1.number_served} customers.")
+# ##############################################################################
+# 9-5
